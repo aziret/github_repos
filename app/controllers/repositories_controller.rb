6549fa1d3d0261e6535b1efbfe16c9b75295bccc
Aziret Alisher <aziretalisher@gmail.com>
2023-04-27 17:07:00 +0100

    Add basic logic for requesting Github repos

---
+++ app/controllers/repositories_controller.rb
@@ -0,0 +1,38 @@
+require 'net/http'
+
+class RepositoriesController < ApplicationController
+  def index
+    @repositories ||= []
+  end
+
+  def search
+    search_term = params[:search_term].to_s.strip
+
+    if search_term.empty?
+      @error_message = 'Please enter a search term'
+      render :index
+    else
+      url = URI.parse("https://api.github.com/search/repositories?q=#{search_term}")
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      request = Net::HTTP::Get.new(url.request_uri)
+      response = http.request(request)
+      data = JSON.parse(response.body)
+
+      if response.code.to_i == 200
+        @repositories = data['items']
+      else
+        @error_message = 'Failed to fetch repositories. Please try again later.'
+      end
+
+      if @repositories
+        respond_to do |format|
+          format.html { render :index }
+          format.turbo_stream
+        end
+      else
+        redirect_to root_path, locals: { error_message: @error_message }
+      end
+    end
+  end
+end
---
+++ spec/requests/repositories_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Repositories", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST /search", vcr: { cassette_name: 'requests/repositories' } do
+    it "appends the result to the frame" do
+      post "/repositories/search", params: { search_term: 'octokit'}, as: :turbo_stream
+      expect(response.media_type).to eq(Mime[:turbo_stream])
+      expect(response.body).to include('<turbo-stream action="append" target="repositories">')
+    end
+  end
+end
